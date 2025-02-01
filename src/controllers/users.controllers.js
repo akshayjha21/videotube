9a3233ff34159145f7333466dafbf2a588dfe776
akshay <akshayjha2111@gmail.com>
2025-02-01 10:31:23 +0530

    adding files

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,7 +1,7 @@
 import {apierror} from "../utils/apierror.js"
 import {asynchandler} from "../utils/asynchandler.js";
 import {apiresponse} from "../utils/apiresponse.js";
-import{user} from "../models/user.models.js";
+import {user} from "../models user.models.js";
 import {uploadcloudinary} from "../utils/cloudinary.js"
 
 const registerUser=asynchandler(async(req,res)=>{
@@ -14,18 +14,40 @@ const registerUser=asynchandler(async(req,res)=>{
         
     }
     //validation if user existed or not
-    const existeduser=user.findOne({
+    const existeduser=await user.findOne({
         $or:[{email}, {username}]
     })
     if(existeduser){
         throw new apierror(409,"User already existed")
     }
-    const avatarlocalpath=req.files?avatar[0]?.path;
-    const coverlocalpath=req.files?coverimage[0]?.path;
+    const avatarlocalpath=req.files?.avatar[0]?.path
+    const coverlocalpath=req.files?.coverimage[0]?.path
     if(!avatarlocalpath){
         throw new apierror(400,"Avatar file is missing")
     }
-
+    const avatar=await uploadcloudinary(avatarlocalpath);
+    let coverimage="";
+    if(coverimage){
+        coverimage=await uploadcloudinary(coverimage)
+    }
+    const user=await user.create({
+        fullname,
+        email,
+        avatar:avatar.url,
+        coverimage:coverimage?.url||"",
+        username:username.toLowerCase(),
+        password,
+    })
+    //we have to validating the user
+    const createduser=await user.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    if(!createduser){
+        throw new apierror(500,"Something went wrong while registering a user");
+    }
+    return res
+    .status(201)
+    .json(new apiresponse(200,createduser))
 })  
 export {
     registerUser
